d6c6a24f30f47780dbddceed2bef90f34cabae2f
manases315 <manasescastroyol@gmail.com>
2019-05-20 16:42:53 -0600

    hola

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -140,4 +140,126 @@ sineCanvas.addChild(
   $scope.settings = {
     enableFriends: true
   };
+  $(document).ready(function(){
+    
+var checkArray = []; // para verificar si las dos cartas con click son el mismo personaje
+var idArray = []; //array para guardar los ids de las cartas que tienen click 
+var contador = 0;
+var fin = 0; 
+var fields = document.querySelectorAll(".atras");
+
+
+var images = [
+"http://www.askabide.com/img/servicios/condones.jpg",
+"https://lahoja.mx/images/17-03/epidemia.jpg",
+"https://static2.laverdad.es/www/pre2017/multimedia/noticias/201702/15/media/108747761.jpg",
+"https://endimages.s3.amazonaws.com/cache/a7/dd/a7dd49cc9fa82674401bece11e51fc45.jpg",
+"https://i.pinimg.com/originals/17/c4/2b/17c42bbb9d7b4cb65167c6a7778d63a6.jpg",
+"https://demorelos.com/wp-content/uploads/2018/11/condones-e-implantes-metodos-anticonceptivos-para-jovenes.png",
+"https://image.posta.com.mx/sites/default/files/styles/full_article_phone_1x/public/prueban-con-exito-en-hombres-sanos-una-nueva-pildora-anticonceptiva-masculina.jpg",
+"https://s-media-cache-ak0.pinimg.com/originals/74/11/00/74110094c67aa9a519c70ecb7cec7d76.png",
+"http://diysolarpanelsv.com/images/neville-clipartpng-4.",
+"https://ae01.alicdn.com/kf/HTB1QyQEaU_rK1Rjy0Fcq6zEvVXaL/Durex-condones-32-unids-caja-de-l-tex-Natural-suave-lubricado-anticonceptivos-4-tipos-preservativos-para.jpg_640x640.jpg",
+"https://lahoja.mx/images/17-03/epidemia.jpg",
+"https://static2.laverdad.es/www/pre2017/multimedia/noticias/201702/15/media/108747761.jpg",
+"https://endimages.s3.amazonaws.com/cache/a7/dd/a7dd49cc9fa82674401bece11e51fc45.jpg",
+"https://i.pinimg.com/originals/17/c4/2b/17c42bbb9d7b4cb65167c6a7778d63a6.jpg",
+"https://demorelos.com/wp-content/uploads/2018/11/condones-e-implantes-metodos-anticonceptivos-para-jovenes.png",
+"https://image.posta.com.mx/sites/default/files/styles/full_article_phone_1x/public/prueban-con-exito-en-hombres-sanos-una-nueva-pildora-anticonceptiva-masculina.jpg",
+"https://s-media-cache-ak0.pinimg.com/originals/74/11/00/74110094c67aa9a519c70ecb7cec7d76.png",
+"https://ae01.alicdn.com/kf/HTB1QyQEaU_rK1Rjy0Fcq6zEvVXaL/Durex-condones-32-unids-caja-de-l-tex-Natural-suave-lubricado-anticonceptivos-4-tipos-preservativos-para.jpg_640x640.jpg"
+];
+// verificacion de los clicks
+function clicked() { 
+    if ($(this).find(".inner-wrap").hasClass("flipped")) {
+        return;
+    }
+    $(this).find(".inner-wrap").toggleClass("flipped");
+    checkArray.push($(this).find("img").attr("src"));
+    idArray.push($(this).attr("id"));
+    check();
+}
+
+$(".carta").on("click", clicked);
+    
+//reiniciar el juego
+function reiniciar() {
+    $(".atras").find("img").remove(); //quitar todas las imagenes actuales
+    $(".carta .inner-wrap").removeClass("flipped"); // quitar la classe flipped para volver a su estado inicial
+    checkArray = []; 
+    idArray = [];
+    contador = 0; 
+    fin = 0;
+    iniciarJuego();
+}
+//para verificar el fin del juego
+function verificarFin() {
+    if (fin === 18) { //si todas las cartas estan volteadas
+        alert("Juego finalizado, lo has logrado en " + contador + " intentos");
+        reiniciar();
+    }
+}
+//para random de las imagenes 
+function shuffleArray(array) { 
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+function iniciarJuego() {
+
+    
+
+    var arr = shuffleArray(images); //array con las imagenes de forma aleatoria
+ // append de las imagenes a la clase para la parte de atras de las cartas
+    for (var i = 0; i < fields.length; i++) {
+        var img = document.createElement("img");
+        img.src = arr[i];
+        fields[i].appendChild(img);
+    }
+
+
+}
+
+function check() {
+  //si los fields se  han hecho dos clicks 
+    if (checkArray.length === 2) {
+        $(".carta").off("click", clicked); 
+        setTimeout(function(){
+      //si no hay match
+            if (checkArray[0] !== checkArray[1]) { 
+        //voltear las dos cartas seleccionadas
+                $("#" + idArray[0]).find(".inner-wrap").removeClass("flipped"); 
+                $("#" + idArray[1]).find(".inner-wrap").removeClass("flipped"); 
+                contador++;
+                //vaciar los arrays para la siguiente eleccion
+        checkArray = []; 
+                idArray = []; 
+        //habilitar el click de nuevo
+                $(".carta").on("click", clicked);
+            } else {
+
+                contador++;
+        
+                fin += 2; // contador para el final del juego, se agregan dos para el contador de fin
+                //vaciar los dos arrays
+        checkArray = []; 
+                idArray = []; 
+                verificarFin(); 
+                $(".carta").on("click", clicked); 
+            }
+            document.querySelector(".counter").innerHTML = contador;
+        }, 800);    
+    }
+}
+
+
+
+iniciarJuego();
+
+});
 });
